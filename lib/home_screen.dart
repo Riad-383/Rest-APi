992daf28cce9829a570aedc2cbb6715ccfe8e02b
Riad <mriad0096@gmail.com>
2023-05-20 22:12:25 +0600

    change

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -24,7 +24,7 @@ class _HomeScreenState extends State<HomeScreen> {
             bottom: const TabBar(
               tabs: [
                 Tab(
-                  child: Icon(Icons.groups),
+                  child: Icon(Icons.groups  ),
                 ),
                 Tab(
                   child: Text('Chats'),
